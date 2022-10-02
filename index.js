315808da2bdd789cc60e7c3e6fb1cc1d84c954e7
Phillip Rose <omnomcake3549@gmail.com>
2022-10-02 00:31:22 -0400

    Results Save
    Messages now call stored proc to save results.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ConnectionService } = require('discord.js');
+const mySql = require('mysql');
+var dbConnection;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, 
@@ -8,14 +10,33 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 client.on('ready', () => {
     console.log('Logged in as ' + client.user.tag + '!');
+
+    dbConnection = mySql.createConnection({
+        host:process.env.DB_HOST,
+        port:process.env.DB_PORT,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_NAME
+    });
+
+    dbConnection.connect();
 });
 
 client.on('messageCreate', msg => {
     // You can view the msg object here with console.log(msg)
     console.log(msg)
      if (msg.content.includes('Framed #') && (msg.content.includes('🟥') || msg.content.includes('🟩'))) {
-       msg.reply(`Hello ${msg.author.username}`);
-       msg.reply('Miss Count: ' + (msg.content.match(/🟥/g) || []).length);
+        var userId = msg.author.id;
+        var userName = msg.author.username;
+        var score = (msg.content.match(/🟥/g) || []).length;
+        var index = msg.content.indexOf('#');
+        var gameNum = msg.content.substring(index+1, index+4);
+
+        // msg.reply(`Hello ${msg.author.username}`);
+        // msg.reply('Miss Count: ' + (msg.content.match(/🟥/g) || []).length);
+        
+        var query = 'call sp_addScore(\'' + userId + '\', \'' + userName + '\', ' + gameNum + ', ' + score + ', @result);'
+        dbConnection.query(query);
      }
     });
 
